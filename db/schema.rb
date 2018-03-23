fd2896f4c6a6b0c800d217175c32c71ddddd2e72
tblessing1s <tblessing>
2018-03-22 20:12:51 -0500

    App Catchup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319233335) do
+ActiveRecord::Schema.define(version: 20180323004214) do
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180319233335) do
     t.integer "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "manager", default: false
   end
 
   create_table "tasks", force: :cascade do |t|
