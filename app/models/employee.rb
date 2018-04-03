153ec9cc0cf442606e56f5f9168e4293cca2ca70
JulieRybolt <jrybolt73@gmail.com>
2018-04-02 19:42:01 -0500

    seed file, manager form

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,6 @@
 class Employee < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true,
