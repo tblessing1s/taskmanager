f1f3b8f90afd99435c68a6c05eb70ef652f6fbd2
tblessing1s <32023037+tblessing1s@users.noreply.github.com>
2018-04-16 17:43:51 -0500

    Revert "Manager"

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,28 +1,19 @@
 class TasksController < ApplicationController
-<<<<<<< HEAD
-  before_action :require_signin, except: [:index, :new, :edit, :create, :update]
-  before_action :require_manager
-=======
   before_action :require_signin, except: [:index]
   before_action :require_manager, except: [:index, :show]
   before_action :set_task, except: [:index, :new, :create]
->>>>>>> 517ac87b0e3ebf309e92bff9916d8ecefebfcc5b
 
   def index
     @tasks = Task.all
   end
-
   def show
     @comments = @task.comments
   end
-
   def new
     @task = Task.new
   end
-
   def edit
   end
-
   def update
       if @task.update(task_params)
         redirect_to tasks_path, notice: "Task Updated"
@@ -30,12 +21,10 @@ class TasksController < ApplicationController
         render :new
       end
   end
-
   def destroy
     @task.destroy
     redirect_to tasks_path, notice: 'Task Deleted'
   end
-
   def create
     @task = Task.new(task_params)
     if @task.save
@@ -51,10 +40,6 @@ private
   end
   def task_params
     params.require(:task).
-<<<<<<< HEAD
-      permit(:title, :description, :lot, :reoccuring, :due, :employee_id)
-=======
       permit(:title, :description, :lot, :reoccuring, :due, :employee_id, :completed)
->>>>>>> 517ac87b0e3ebf309e92bff9916d8ecefebfcc5b
   end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -9,7 +9,6 @@ class Employee < ApplicationRecord
   validates :email, presence: true,
                   format: /\A\S+@\S+\z/,
                   uniqueness: { case_sensitive: false }
-  has_many :tasks, dependent: :destroy
 
   def self.authenticate(email, password)
     employee = Employee.find_by(email: email)
