724c645edccd2ead1d2128bd5f808db5aabc1370
LupercusWhitewolf <34462063+LupercusWhitewolf@users.noreply.github.com>
2018-04-16 20:16:41 -0500

    Task Querys Created
    Created Querys for Tasks. Querys added to the Employee page, querys limited to 5 most recent results. Added a partial document called _manager.html.erb, this show document will be for seperating the differences in the employee and manager dashboards.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,13 @@ class Task < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :employee
 
+  def self.completed
+    Task.where(:completed => true).order("updated_at desc").take(5)
+  end
+  def self.current
+    Task.where('due = ?', Date.current).order("title desc").take(5)
+  end
+  def self.upcoming
+    Task.where('due >= ?', Date.current).order("due desc").take(5)
+  end
 end
